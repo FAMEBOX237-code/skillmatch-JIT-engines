d241abc2b79ff5c6f4322f8fa8a9e0740e98815a
MARK AJINWI NDITAPAH <nditapah.mark@ictuniversity.edu.cm>
2026-01-20 20:16:41 +0100

    added css and some other modifications

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ def get_db_connection():
     return pymysql.connect (
         host = 'localhost', 
         user='root',
-        password='skilmatch123',
+        password='skillmatch123',
         database='skillmatch_db',
         cursorclass=pymysql.cursors.DictCursor
     )
