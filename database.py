95a19c487cec1b5dd8fd4332678ce820c477b893
Lankoko Ayinda Raissa <lankokoayinda@gmail.com>
2026-01-21 16:09:54 +0100

    buttons linking

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ def get_db_connection():
     return pymysql.connect (
         host = 'localhost', 
         user='root',
-        password='yann@237python',
+        password='Raissa200619',
         database='skillmatch_db',
         cursorclass=pymysql.cursors.DictCursor
     )
